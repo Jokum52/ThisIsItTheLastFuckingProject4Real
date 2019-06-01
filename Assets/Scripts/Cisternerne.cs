ad71f0929052995bd8d37436589b0b981fea1d57
Mus <guggiolsen@gmai.com>
2019-06-01 23:08:18 +0200

    wwise act 3

---
+++ Assets/Scripts/Cisternerne.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cisternerne : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject point2;
+    public float distance2_;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        distance2_ = Vector3.Distance(player.transform.position, point2.transform.position);
+        
+        AkSoundEngine.SetRTPCValue("wind_cave_opening", distance2_);
+       Debug.Log("distance is " + distance2_);
+    }
+
+
+}
