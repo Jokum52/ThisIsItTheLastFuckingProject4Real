e0bc2d88fb4dd48f03159bc837a33871ef513474
Mus <guggiolsen@gmai.com>
2019-06-01 20:13:53 +0200

    ACt1 tower scene wwise stuff

---
+++ Assets/Scripts/Elevator_sound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator_sound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void elevatorstart()
+    {
+
+        AkSoundEngine.PostEvent("elevator_start", gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/elevator_stop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elevator_stop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void elevatorstop()
+    {
+
+        AkSoundEngine.PostEvent("elevator_stop", gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
