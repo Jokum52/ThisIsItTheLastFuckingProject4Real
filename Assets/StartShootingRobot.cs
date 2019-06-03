e3f9cc06dc4cbbee0a20e3d06c898ddbf3e23049
Casper Olsen <guggiolsen@gmail.com>
2019-06-03 18:55:46 +0200

    Robot act 3 triggers yay

---
+++ Assets/StartShootingRobot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartShootingRobot : MonoBehaviour
+{
+
+
+    Animator m_Animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        m_Animator = GetComponent<Animator>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+
+        {
+
+            m_Animator.SetBool("Activate", true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
