46145a5e1a5f24dd5bfc4a07d3b55ef73de1d9dc
Mus <guggiolsen@gmai.com>
2019-06-08 23:09:19 +0200

    wwise cave - kind of a mess...

---
+++ Assets/Scripts/CaveFootstepReverb.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveFootstepReverb : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject point_reverb;
+    public float distance_reverb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        distance_reverb = Vector3.Distance(player.transform.position, point_reverb.transform.position);
+
+        AkSoundEngine.SetRTPCValue("footstep_reverb", distance_reverb);
+        Debug.Log("distance is " + distance_reverb);
+    }
+
+
+}
