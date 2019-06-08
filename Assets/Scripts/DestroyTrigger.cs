b7b418a62cc56f2d885b13620bceeb8cafa111f3
Casper Olsen <guggiolsen@gmail.com>
2019-06-08 22:25:25 +0200

    RobotShutDown Done (sound missing)

---
+++ Assets/Scripts/DestroyTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTrigger : MonoBehaviour
+{
+
+    /* void OnCollisionEnter(Collision other)
+
+     {
+
+         if (other.gameObject.tag.Equals("Player"))
+
+         {
+
+             gameObject.SetActive(false);
+
+         }
+         */
+
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+
+            Object.Destroy(gameObject, 0.5f);
+
+    }
+}
