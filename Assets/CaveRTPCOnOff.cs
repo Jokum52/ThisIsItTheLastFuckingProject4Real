f72faf41bce0501f62b867521fc1523586fe85ea
Mus <guggiolsen@gmai.com>
2019-06-09 23:20:20 +0200

    wwise for the day

---
+++ Assets/CaveRTPCOnOff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveRTPCOnOff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AkSoundEngine.SetRTPCValue("cave_reverb_on_off", 0, gameObject);
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
