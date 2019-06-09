f421cc3985238d19e4b14cd86e14a01c4186013a
Casper Olsen <guggiolsen@gmail.com>
2019-06-09 21:09:48 +0200

    Act1 intro done

---
+++ Assets/StarLevel1Animation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarLevel1Animation : MonoBehaviour
+{
+
+    public GameObject otherObject;
+    public Animator otherAnimator;
+    public GameObject otherObject2;
+    public Animator otherAnimator2;
+
+
+
+    // Animator m_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // anim = GetComponentInChildren<Animator>();
+
+        // GetComponent<Animator>().enabled = false;
+    }
+
+    void Awake()
+    {
+        otherAnimator = otherObject.GetComponent<Animator>();
+        otherAnimator2 = otherObject2.GetComponent<Animator>();
+
+    }
+
+
+
+    private void StartGame1()
+    {
+        
+            otherAnimator2.SetTrigger("EntryJump");
+            otherAnimator.SetBool("Deactivate", true);
+
+
+        
+    }
+
+
+
+
+}
