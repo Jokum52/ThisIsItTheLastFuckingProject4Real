2da85b90aaad7a6b6119bc3a46c84156cb255939
Casper Olsen <guggiolsen@gmail.com>
2019-06-09 23:01:55 +0200

    Started on act 4 ending

---
+++ Assets/EndGameAnims.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameAnims : MonoBehaviour
+{
+
+    public GameObject otherObject;
+    public Animator otherAnimator;
+    public GameObject otherObject2;
+    public Animator otherAnimator2;
+
+   
+
+    // Animator m_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // anim = GetComponentInChildren<Animator>();
+
+        // GetComponent<Animator>().enabled = false;
+    }
+    
+    void Awake()
+    {
+        otherAnimator = otherObject.GetComponent<Animator>();
+        otherAnimator2 = otherObject2.GetComponent<Animator>();
+
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+
+        {
+            otherAnimator.SetTrigger("End");
+            otherAnimator2.SetTrigger("KillPortal");
+           
+
+        }
+    }
+
+  
+
+    
+}
