dbde195c1c8b0bfd69db16ef120788b773d4237d
Mus <guggiolsen@gmai.com>
2019-06-10 21:42:42 +0200

    All kinds of shit (Act 2, Hangar)

---
+++ Assets/MudRTPC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MudRTPC : MonoBehaviour
+{
+    public float mudRTPC = 10; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+
+            AkSoundEngine.SetRTPCValue("mud", mudRTPC); ;
+
+    }
+}
