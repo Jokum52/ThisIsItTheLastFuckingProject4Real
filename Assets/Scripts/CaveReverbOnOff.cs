71437887bf9a4b15c914f803639c72350d1feeb5
TurboSpasser <denvamlefinger@hotmail.com>
2019-06-11 18:36:33 +0200

    pull nu for hælvæd

--- Assets/Scripts/CaveReverbOnOff.cs
+++ Assets/Scripts/CaveReverbOnOff.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class CaveReverbOnOff : MonoBehaviour
 {
+    public float caveReverb = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AkSoundEngine.SetRTPCValue("cave_reverb_on_off", caveReverb);
     }
 
     // Update is called once per frame
