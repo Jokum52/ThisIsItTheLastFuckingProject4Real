60f42c6fce068843d3f0c647cbd95f2485167d75
TurboSpasser <denvamlefinger@hotmail.com>
2019-06-12 21:19:47 +0200

    Introscene kewl sounds okay dokey

---
+++ Assets/WindowSounds.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowSounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void musicIntro()
+    {
+
+        AkSoundEngine.SetState("Chase", "Intro");
+    }
+
+    private void musicMain()
+    {
+
+        AkSoundEngine.SetState("Chase", "Main");
+    }
+
+    private void windowCrush()
+    {
+
+        AkSoundEngine.PostEvent("window_crush", gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
