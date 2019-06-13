d04baceca352ea69bad9bc71d3c2b7dc27567870
TurboSpasser <denvamlefinger@hotmail.com>
2019-06-13 21:38:57 +0200

    hammer og svejs

---
+++ Assets/HammerSounds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerSounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void hammer()
+    {
+
+        AkSoundEngine.PostEvent("Hammer2", gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
